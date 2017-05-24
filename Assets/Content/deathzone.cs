f5b094763a72e5dbadd525a3f595610f573654cf
Your Name <you@example.com>
2017-05-24 14:21:02 +0300

    hw2

---
+++ Assets/Content/deathzone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathzone : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		rabbittosha rabit = collider.GetComponent<rabbittosha>();
+		if (rabit != null)
+		{
+			levelcontroller.current.onRabitDeath(rabit);
+		}
+	}
+}
---
+++ Assets/Content/levelcontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelcontroller : MonoBehaviour {
+
+	public static levelcontroller current;
+
+	void Awake()
+	{
+		current = this;
+	}
+
+	Vector3 startingPosition;
+	public void setStartPosition(Vector3 pos)
+	{
+		this.startingPosition = pos;
+	}
+	public void onRabitDeath(rabbittosha rabit)
+	{
+		rabit.transform.position = this.startingPosition;
+	}
+}
